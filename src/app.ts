b2d981fc10547d2cb9befb976e6164022bdf2079
Timur Kaiser <timurkayzer@gmail.com>
2023-06-04 19:05:39 +0500

    sequence

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,17 @@ import { dbConnect } from "./db-connect";
 import { Customer } from "./models/customer.model";
 import { UserService } from "./services/user.service";
 const userService = new UserService;
-dbConnect();
 
-setInterval(async () => {
-    try {
-        let customers = userService.generateFakeUsers(200);
-        await Customer.insertMany(customers);
-    }
-    catch (e) {
-        console.error(e?.toString());
-    }
-}, 1000);
+dbConnect().then(() => {
+    setInterval(async () => {
+        try {
+            let customers = userService.generateFakeUsers(200);
+            await Customer.insertMany(customers);
+        }
+        catch (e) {
+            console.error(e?.toString());
+        }
+    }, 1000);
+})
+    .catch(e => console.error(e?.toString()));
+
